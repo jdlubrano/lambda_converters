bb14955510c5677e55abb9905e2c569ba4332ea4
Joel Lubrano <joel.lubrano@gmail.com>
2016-10-30 12:51:05 -0400

    Update zip script

--- step_to_stl/zip.sh
+++ step_to_stl/zip.sh
@@ -2,7 +2,7 @@
 
 # Creates a zip file that can be deployed as an AWS Lambda function
 
-zip -r9 step_geometry_lambda.zip \
+zip -r9 step_to_stl_lambda.zip \
 aws_s3.py \
 conversion_error.py \
 handler.py \
