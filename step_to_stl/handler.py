24abf09d4c3edb77860e0270e6655055b4d4d779
Joel Lubrano <joel@maketime.io>
2016-11-04 16:36:16 -0400

    Derive destination bucket from source bucket

--- step_to_stl/handler.py
+++ step_to_stl/handler.py
@@ -2,13 +2,15 @@ from __future__ import print_function
 
 import json
 import os
-
+import re
 
 from aws_s3 import AwsS3
 from conversion_error import ConversionError
 from step_to_stl import convert
 
 STEP_KEY_PREFIX = 'cad_files/stl/'
+def stl_key(step_object):
+    return re.sub(r'/(stp|step)/', 'stl', step_object)
 
 def lambda_handler(event, context):
     print('Running lambda_handler...')
@@ -37,7 +39,7 @@ def lambda_handler(event, context):
     print('Converting {} to {}'.format(local_step, stl_file))
     convert(local_step, stl_file)
 
-    s3_key = STEP_KEY_PREFIX + os.path.basename(stl_file)
+    s3_key = stl_key(s3_object)
 
     print('Uploading {} to {}/{}'.format(stl_file, s3_bucket, s3_key))
     s3.put_object(stl_file, s3_bucket, s3_key)
